02a0b10680ad3ecbe86460ab14601a2d016f21b1
Rishabh5971 <rishabhkumar5971@gmail.com>
2023-10-05 11:03:29 +0530

    First Commit

---
+++ print1.java
@@ -0,0 +1,8 @@
+// Write a program to print "Ram Ram".
+
+public class print1{
+	public static void main(String args[]){
+		System.out.println("Radha Radha");
+		System.out.println("Ram Ram");
+	}
+}
---
+++ print2.java
@@ -0,0 +1,8 @@
+// Write a program to print "My Name Is Rishabh Kumar." and "Radha Radha".
+
+public class print2{
+	public static void main(String args[]){
+		System.out.println("My Name Is Rishabh Kumar.");
+		System.out.println("Radha Radha");
+	}
+}
